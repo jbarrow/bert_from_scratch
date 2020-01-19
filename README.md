8b6c9bd196ce634961d4a2e99c8e166ce9fc2e82
Joe Barrow <joseph.d.barrow@gmail.com>
2020-01-18 23:47:06 -0500

    Tested conversion script

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# structurebert
+# bert_from_scratch
+
+This directory contains all the scripts and code you need to train BERT from scratch on a TPU on Google Cloud.
--- scripts/convert.sh
+++ scripts/convert.sh
@@ -3,15 +3,16 @@
 # $1 - bucket name
 
 bucket_name=$1
+checkpoint_num=1000000
 
-mkdir $bucket_name $bucket_name/tf1.0 $bucket_name/pytorch
+#mkdir $bucket_name $bucket_name/tf1.0 $bucket_name/pytorch
 
-gsutil -m cp gs://$bucket_name/tf1.0 $bucket_name/tf1.0
+#gsutil -m cp -r gs://$bucket_name/tf1.0 $bucket_name
 
 cp $bucket_name/tf1.0/vocab.txt $bucket_name/pytorch/vocab.txt
 cp $bucket_name/tf1.0/bert_config.json $bucket_name/pytorch/config.json
 
 python3 -m transformers.convert_bert_original_tf_checkpoint_to_pytorch \
-  --tf_checkpoint_path $bucket_name/tf1.0/model.ckpt \
-  --bert_config_file $bucket_name/bert_config.json \
+  --tf_checkpoint_path $bucket_name/tf1.0/pretraining_output/model.ckpt-$checkpoint_num \
+  --bert_config_file $bucket_name/tf1.0/bert_config.json \
   --pytorch_dump_path $bucket_name/pytorch/pytorch_model.bin
